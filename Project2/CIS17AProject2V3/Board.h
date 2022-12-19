3c1f301a5a5549f169559b535619a859ef1669ae
Alejandro Porter <107812659+aporter30@users.noreply.github.com>
2022-12-18 17:29:15 -0800

    Create Board.h

---
+++ Project2/CIS17AProject2V3/Board.h
@@ -0,0 +1,40 @@
+/* 
+ * File:   Board.h
+ * Author: ajporter
+ *
+ * Created on December 10, 2022, 2:41 PM
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <map>
+#include <iterator>
+#include <string>
+#include "Deck.h"
+using namespace std;
+
+class Board {
+    public:
+        Board();
+        virtual ~Board();
+        void setSpecialCards();
+        void setBoard();
+        void outputBoard();
+        int getboardSize();
+        int getCastleDistance(int location);
+        int getLocation(string card, int prevLocation, string);
+        bool ifSpecialCard(string card);
+        bool winner(string, int);
+        int doub(string, int);
+        int sing(string, int);
+    private:
+        int boardSize;  //how many spaces there are on the board
+        string board[134];    //holds the varying positions on the board
+        int castleDistance; //subtracts the players
+        string colors[6];   //colors for cards
+        map<string, int>specialCards;
+        int location;
+};
+
+#endif /* BOARD_H */
